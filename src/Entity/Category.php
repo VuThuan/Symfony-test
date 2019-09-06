b08d7a614614eee0842d6cfd71049817037760f1
vuthuan <vuthuan3090@gmail.com>
2019-09-06 11:20:50 +0700

    add data MVC

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,7 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity()
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
  * @ORM\Table(name="categories")
  */
 class Category
@@ -114,6 +114,16 @@ class Category
         $this->affiliates->removeElement($affiliate);
 
         return $this;
+    }
+
+    /**
+     * @return Job[]|ArrayConnection
+     */
+    public function getActiveJobs()
+    {
+        return $this->jobs->filter(function (Job $job) {
+            return $job->getExpiresAt() > new \DateTime();
+        });
     }
 
     /**
